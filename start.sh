86fa149c6aa66d9ac7393d9a8f8bf68d2f9399a8
dd-Splunk <ddessy@splunk.com>
2023-09-05 09:41:15 +0200

    Adding player and exit logic

--- start.sh
+++ start.sh
@@ -1,2 +1,28 @@
-#/bin/bash
-python F1_2022_Conference_ingest.py --hostname pi-f1 --o11y yes --splunk yes --mode solo --player EventPrefix_PlayerName
+#!/bin/bash
+
+# type "exit" to exit
+
+stty -echoctl # hide ^C
+
+# function called by trap
+ctrl_c() {
+    tput setaf 1
+    printf "\rSIGINT caught      "
+    tput sgr0
+    sleep 1
+    printf "\rEnter player name (exit) >>> "
+}
+
+trap 'ctrl_c' SIGINT
+
+event="pi-f1"
+player=""
+
+while true
+do
+    printf "\rEnter player name (exit) >>> "
+    read  player
+    [[ $player == exit ]] && break
+    python3 F1_2022_Conference_ingest.py --hostname pi-f1 --o11y yes --splunk yes --mode solo --player ${event}_${player}
+
+done
